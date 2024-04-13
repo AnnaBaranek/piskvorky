e868dc64cce237ccaa4261b3a8f95cb380653d9e
Anna Baranek <annaxbaranek@gmail.com>
2024-04-14 00:32:31 +0200

    pridan eventListener na vsechny btns

--- hra.js
+++ hra.js
@@ -19,16 +19,12 @@ const selectButton = (event) => {
     }
 };
 
-document.querySelector('button:nth-child(1)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(2)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(3)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(4)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(5)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(6)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(7)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(8)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(9)').addEventListener('click', selectButton)
-document.querySelector('button:nth-child(10)').addEventListener('click', selectButton)
+
+const allButtons = document.querySelectorAll('.btn-field');
+allButtons.forEach((button) => {
+    button.addEventListener('click', selectButton);
+});
+
 
 
 const restartLink = document.querySelector('.icon-restart');
